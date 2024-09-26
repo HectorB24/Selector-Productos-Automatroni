07869aeaf9c6e3d4fcb93b85d58c61142a368f51
Héctor Bolívar <hectorbc338@gmail.com>
2024-09-25 21:28:16 -0400

    modificación de Styles en viñetas

--- src/Pages/Components/Eyelashes/index.jsx
+++ src/Pages/Components/Eyelashes/index.jsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 
 const Eyelashes = () => {
 
-    const activeStyle = 'text-base text-white text-center p-2 mr-2 bg-[#0000c8] border-[1px] border-[#0000c8] rounded-t-[0.6rem] transition-all duration-500 ease-in-out cursor-pointer';
-    const inactiveStyle = 'bg-white text-black border-solid border-l-[2px] border-r-[2px] border-b-0 border-[#d5d7d8]';
+    const activeStyle = 'bg-white text-black';
+    const inactiveStyle = 'bg-[#0000c8] text-white border-solid border-4 border-{#d5d7d8} p-1';
     return (
         <div className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
             <ul className='flex items-center gap-3'>
